f959b5a06316648758db21db7f22e444e25338b4
shrish <shrishsinha20000@gmail.com>
2024-06-22 08:42:17 +0530

    project 1 finished

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Center(
+                child: Text(
+                    'I am Rich',
+                style: TextStyle(color: Colors.white),
+                ),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage(
+                'images/diamond_.png'
+            ),
+          ),
+          ),
+      ),
+    ),
+  );
+}
